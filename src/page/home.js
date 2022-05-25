d87ee537be4d4a21c34fe08cd2717eb27aa9aa0b
jokojr97 <joko.riyadi97@gmail.com>
2022-05-25 16:15:00 +0700

    setting router

---
+++ src/page/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = props => {
+    console.log("halaman home");
+    return <div>
+        {
+            <div>
+                ini adalah Halaman Home
+            </div>
+        }
+    </div>
+
+}
+
+export default Home;
---
+++ src/page/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = props => {
+
+    return <div> {
+        <div>
+            ini adalah halaman login
+        </div>
+    }</div>
+}
+
+export default Login
