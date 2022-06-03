0b5ab7a8080fe236101f5f681a2618b7a7d327ab
jokojr97 <joko.riyadi97@gmail.com>
2022-06-03 20:18:00 +0700

    update cors

--- src/page/home.js
+++ src/page/home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import '../index.css';
 import AboutMe from "./_partials/aboutme";
 import Banner from "./_partials/banner";
@@ -9,6 +10,17 @@ import NavMenu from "./_partials/navbar";
 
 const Home = props => {
     // console.log("halaman home");
+    const url = "https://backend-portojoe.netlify.app/";
+    const [portofolios, setPortofolios] = useState(null);
+    useEffect(() => {
+        axios.get(url).then(res => {
+            setPortofolios(res.data);
+        })
+    }, [url])
+
+
+
+
     const datapost = [{
         title: "judul 1 Lorem ipsum dolor sit, amet consectetur",
         image: "jr.png",
@@ -25,6 +37,8 @@ const Home = props => {
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere optio blanditiis perferendis! Aperiam enim at excepturi optio iusto quaerat architecto perferendis quam labore atque modi sit, temporibus blanditiis cumque dolores.",
         link: "#"
     }]
+
+    // datapost = portofolios;
     return <div>
         {
             <div>
